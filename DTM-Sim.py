46e3f51ee81fe13c7329e33915929d440d9d90b2
lamlinda <linda.nguyen0819@gmail.com>
2025-06-26 01:02:50 -0700

    Move part A DTM to its own function so part b can be implemented easier

--- DTM-Sim.py
+++ DTM-Sim.py
@@ -8,43 +8,71 @@
 
 
 #6-tuple that defines the DTM
-inputSymbols = { 0, 1 } #sigma
-allSymbols = { '0', '1', 'b' } #gamma
-states = { "q0", "q1", "q2", "q3", "qY", "qN" }
-direction = { -1, 1 } #left = -1, right = +1: this is contained within the transition table
-blankSymbol = 'b'
+mInputSymbols = {} #sigma
+mAllSymbols = {} #gamma
+mStates = {}
+mDirection = { -1, 1 } #left = -1, right = +1: this is contained within the transition table
+mBlankSymbol = 'b'
 
 #Transition table
-transitions = {
-    'q0': {
-        '0': ('q0', '0', +1),
-        '1': ('q0', '1', +1),
-        'b': ('q1', 'b', -1)
-    },
-    'q1': {
-        '0': ('q2', 'b', -1),
-        '1': ('q3', 'b', -1),
-        'b': ('qN', 'b', -1)
-    },
-    'q2': {
-        '0': ('qY', 'b', -1),
-        '1': ('qN', 'b', -1),
-        'b': ('qN', 'b', -1)
-    },
-    'q3': {
-        '0': ('qN', 'b', -1),
-        '1': ('qN', 'b', -1),
-        'b': ('qN', 'b', -1)
-    }
-}
+mTransitions = {}
 
 #starting state
-startState = 'q0'
-haltStates = { "qY", "qN" }
-tape = ['b'] * 20 #inital tape containing 100 blank symbols
-tapeHistory = []
+mStartState = 'q0'
+mHaltStates = {}
+mTape = ['b'] * 20 #inital tape containing 100 blank symbols
+mTapeHistory = []
 
 ##--------------------------------------------------------------------------------------------
+
+##
+# @brief Initialize global variables 
+#
+# Set the global variables containing the 6-tuple for the DTM. 
+# THIS DTM IS FOR PART A
+#
+# @param input - N/A
+# @return N/A
+def initializeDTMForPartA():
+
+    #get access to the global variables
+    global mInputSymbols
+    global mAllSymbols
+    global mStates
+    global mTransitions
+    global mHaltStates
+
+    #6-tuple that defines the DTM
+    mInputSymbols = { 0, 1 } #sigma
+    mAllSymbols = { '0', '1', 'b' } #gamma
+    mStates = { "q0", "q1", "q2", "q3", "qY", "qN" }
+
+    #Transition table
+    mTransitions = {
+        'q0': {
+            '0': ('q0', '0', +1),
+            '1': ('q0', '1', +1),
+            'b': ('q1', 'b', -1)
+        },
+        'q1': {
+            '0': ('q2', 'b', -1),
+            '1': ('q3', 'b', -1),
+            'b': ('qN', 'b', -1)
+        },
+        'q2': {
+            '0': ('qY', 'b', -1),
+            '1': ('qN', 'b', -1),
+            'b': ('qN', 'b', -1)
+        },
+        'q3': {
+            '0': ('qN', 'b', -1),
+            '1': ('qN', 'b', -1),
+            'b': ('qN', 'b', -1)
+        }
+    }
+
+    mHaltStates = { "qY", "qN" }
+
 
 ##
 # @brief Initialize the tape
@@ -61,22 +89,29 @@ def createTape( input ):
 
     #put the input in the tape starting in the middle
     for char in input:
-        tape[initialIndex] =  char
+        mTape[initialIndex] =  char
         initialIndex += 1
 
 
-
+##
+# @brief Performs the transitions on the tape
+#
+# Uses the transition table to modify the tape and transition through states until it
+# reaches a halt state
+#
+# @param input - N/A
+# @return N/A
 def doTransitions():
 
     numTransitions = 0
-    currentState = startState
+    currentState = mStartState
     headIndex = 10 #start at the initial index we set in the createTape function
 
-    while currentState not in haltStates:
-        symbol = tape[headIndex]
+    while currentState not in mHaltStates:
+        symbol = mTape[headIndex]
 
         #get the next state, symbol to write to tape, and where to move tape head
-        transitionResult = transitions[currentState][symbol]
+        transitionResult = mTransitions[currentState][symbol]
 
         #put the transition results into individual variables for readability
         nextState = transitionResult[0]
@@ -85,32 +120,27 @@ def doTransitions():
 
         #update the state, head index, and write symbol to tape
         currentState = nextState
-        tape[headIndex] = writeSymbol
+        mTape[headIndex] = writeSymbol
         headIndex += moveHead
 
         #print the state of the tape and increment numTransitions
-        print(numTransitions, ". ", tape, sep='')
+        print(numTransitions, ". ", mTape, sep='')
         numTransitions += 1
-        tapeHistory.append(tape)
+        mTapeHistory.append(mTape)
 
     #if number of transitions is greater than 30, write the transitions to a file
     if numTransitions > 29:
         with open("dtm_output.txt", "w") as f:
-            for trans in tapeHistory:
+            for trans in mTapeHistory:
                 f.write(''.join(trans))
-
-
-
-
-
 
 
 # Defining main function
 def main():
+    initializeDTMForPartA()
     createTape("10100")
     doTransitions()
     
-
 
 # Using the special variable 
 # __name__
