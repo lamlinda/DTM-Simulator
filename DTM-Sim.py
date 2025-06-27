4cc778ec7aee462c821b5bdb0b1edbabb47ee4fe
lamlinda <linda.nguyen0819@gmail.com>
2025-06-26 22:49:40 -0700

    Add functionality for binary addition

--- DTM-Sim.py
+++ DTM-Sim.py
@@ -20,7 +20,7 @@ mTransitions = {}
 #starting state
 mStartState = 'q0'
 mHaltStates = {}
-mTape = ['b'] * 20 #inital tape containing 100 blank symbols
+mTape = ['b'] * 30 #inital tape containing 100 blank symbols
 mTapeHistory = []
 
 ##--------------------------------------------------------------------------------------------
@@ -75,6 +75,115 @@ def initializeDTMForPartA():
 
 
 ##
+# @brief Initialize global variables 
+#
+# Set the global variables containing the 6-tuple for the DTM. 
+# THIS DTM IS FOR PART B - BINARY ADDITION
+# SEE MY TRANSITION TABLE FOR EXPLANATION ON HOW THIS WORKS
+#
+# @param input - N/A
+# @return N/A
+def initializeDTMForPartB():
+
+    #get access to the global variables
+    global mInputSymbols
+    global mAllSymbols
+    global mStates
+    global mTransitions
+    global mHaltStates
+
+    #6-tuple that defines the DTM
+    mInputSymbols = { '0', '1', '+' } #sigma
+
+    #S = marks the start of the input
+    #X = 1,  Y = 0
+    #Z = carry with a value of 0
+    #R = carry with a value of 1
+    mAllSymbols = { '0', '1', '+', 'b', 'S', 'X', 'Y', 'Z', 'R', 'E' } #gamma
+    mStates = { "q0", "q1", "q2", "q3", "qY" } 
+
+    #Transition table
+    mTransitions = {
+        'q0': {
+            'S': ('q0', 'S', +1),
+            '0': ('q0', '0', +1),
+            '1': ('q0', '1', +1),
+            'X': ('q0', 'X', +1),
+            'Y': ('q0', 'Y', +1),
+            'b': ('q1', 'b', -1),
+            '+': ('q0', '+', +1),
+            'Z': ('q0', '1', +1),
+            'R': ('q0', '1', +1),
+            'E': ('q6', 'E', -1)
+        },
+        'q1': {
+            '0': ('q4', 'b', -1),
+            '1': ('q2', 'b', -1),
+            '+': ('q6', 'E', -1),
+            'E': ('q6', 'E', -1)
+        },
+        'q2': {
+            '0': ('q2', '0', -1),
+            '1': ('q2', '1', -1),
+            '+': ('q3', '+', -1)
+        },
+        'q3': {
+            'S': ('q7', 'X', -1),
+            '0': ('q0', 'X', +1),
+            '1': ('q0', 'Z', +1),
+            'X': ('q3', 'X', -1),
+            'Y': ('q3', 'Y', -1),
+            'Z': ('qC1', 'Y', -1),
+            'R': ('qC1', 'X', -1)
+        },
+        'q4': {
+            '0': ('q4', '0', -1),
+            '1': ('q4', '1', -1),
+            '+': ('q5', '+', -1)
+        },
+        'q5': {
+            'S': ('q7', 'Y', -1),
+            '0': ('q0', 'Y', +1),
+            '1': ('q0', 'X', +1),
+            'X': ('q5', 'X', -1),
+            'Y': ('q5', 'Y', -1),
+            'Z': ('qC2', 'Y', -1),
+            'R': ('qC2', 'X', -1)
+        },
+        'q6': {
+            'S': ('q8', 'b', +1),
+            '0': ('q6', '0', -1),
+            '1': ('q6', '1', -1),
+            'X': ('q6', '1', -1),
+            'Y': ('q6', '0', -1),
+            'Z': ('qC2', 'Y', -1),
+            'R': ('qC2', 'X', -1)
+        },
+        'q7': {
+            'b': ('q0', 'S', +1)
+        },
+        'q8': {
+            '0': ('q8', '0', +1),
+            '1': ('q8', '1', +1),
+            'E': ('q8', 'b', +1),
+            'b': ('qY', 'b', +1)
+        },
+        'qC1': {
+            '0': ('q0', 'Z', +1),
+            '1': ('q0', 'R', +1)
+        },
+        'qC2': {
+            'S': ('q7', 'X', -1),
+            '0': ('q0', 'X', +1),
+            '1': ('q0', 'Z', +1)
+        }
+    }
+
+    mHaltStates = { "qY" }
+
+##--------------------------------------------------------------------------------------------
+
+##
 # @brief Initialize the tape
 #
 # Initializes the tape by placing the input string somewhere in the middle of 
@@ -86,6 +195,10 @@ def createTape( input ):
 
     #set the initial index to 50 so we place the input somewhere in the middle of the tape
     initialIndex = 10
+
+    #if 'S' is in the symbols list, put 'S' at the beginning of the tape
+    if 'S' in mAllSymbols:
+        mTape[initialIndex - 1] = 'S'
 
     #put the input in the tape starting in the middle
     for char in input:
@@ -109,6 +222,8 @@ def doTransitions():
 
     while currentState not in mHaltStates:
         symbol = mTape[headIndex]
+
+        print(currentState, " - ", mTape[headIndex])
 
         #get the next state, symbol to write to tape, and where to move tape head
         transitionResult = mTransitions[currentState][symbol]
@@ -137,8 +252,8 @@ def doTransitions():
 
 # Defining main function
 def main():
-    initializeDTMForPartA()
-    createTape("10100")
+    initializeDTMForPartB()
+    createTape("1101011+1011")
     doTransitions()
     
 
